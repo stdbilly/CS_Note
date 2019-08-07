ef8781af77e12827fdf813c032aca39b93ccf69d
billy <whb227@163.com>
2019-08-08 00:07:02 +0800

    TextQuery

--- mycode/cpp/STL_Container/TextQuery.cc
+++ mycode/cpp/STL_Container/TextQuery.cc
@@ -1,36 +1,36 @@
-#include <iostream>
-#include <string>
-#include <sstream>
 #include <fstream>
+#include <iostream>
 #include <map>
-#include <unordered_map>
 #include <set>
-using std::set;
-using std::map;
-using std::unordered_map;
-using std::string;
+#include <sstream>
+#include <string>
+#include <unordered_map>
+using std::cin;
 using std::cout;
 using std::endl;
-using std::cin;
 using std::ifstream;
 using std::istringstream;
 using std::make_pair;
+using std::map;
+using std::set;
+using std::string;
+using std::unordered_map;
 
 class TextQuery {
-public:
+   public:
     void readFile(const char* filename) {
         ifstream ifs(filename);
-        if(!ifs){
-            cout<<"ifstream open file error"<<endl;
-            return ;
-        }   
+        if (!ifs) {
+            cout << "ifstream open file error" << endl;
+            return;
+        }
         string line;
         string word;
         int i = 1;
-        while(getline(ifs, line)) {
+        while (getline(ifs, line)) {
             _lines[i] = line;
             istringstream iss(line);
-            while(!iss.eof()) {
+            while (!iss.eof()) {
                 iss >> word;
                 _word2Line[word].insert(i);
                 ++_dict[word];
@@ -39,35 +39,36 @@ public:
         }
     }
 
-    void query(const string & word) {
+    void query(const string& word) {
         auto it = _dict.find(word);
-        if(it != _dict.end()) {
+        if (it != _dict.end()) {
             cout << word << " occurs " << _dict[word] << " times" << endl;
             print(_word2Line[word]);
         } else {
             cout << word << " not found" << endl;
         }
     }
-private:
+
+   private:
     template <class Container>
-        void print(const Container& c) {
-            typename Container::const_iterator cit = c.begin();
-            while(cit != c.end()) {
-                cout << "(line " << *cit << ")" << _lines[*cit] << endl;
-                cit++;
-            }
+    void print(const Container& c) {
+        typename Container::const_iterator cit = c.begin();
+        while (cit != c.end()) {
+            cout << "(line " << *cit << ")" << _lines[*cit] << endl;
+            cit++;
         }
-    unordered_map<int,string> _lines;
+    }
+    unordered_map<int, string> _lines;
     map<string, set<int> > _word2Line;
     map<string, int> _dict;
 };
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     string queryWord;
     cout << "please input query word:";
     cin >> queryWord;
     TextQuery tq;
     tq.readFile("china_daily.txt");
-    tq.query(queryWord);            
+    tq.query(queryWord);
     return 0;
 }
