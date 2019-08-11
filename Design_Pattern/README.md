743e278c0ab8233ba82ce4662651aa8959981927
BillyW <whb227@163.com>
2019-08-11 17:04:21 +0800

    updata

--- Design_Pattern/README.md
+++ Design_Pattern/README.md
@@ -1,4 +1,4 @@
-## [单例模式](singleton.cc)
+## [单例模式](singleton.cc)(Singleton Pattern)
 
 #### 应用场景:
 
@@ -12,7 +12,12 @@
 2. [静态成员函数+atexit](Singleton/autoRelease/singleton2.cc)
 3. [pthread_once + atexit](Singleton/autoRelease/singleton3.cc)
 
-## [PIMPL设计模式](PIMPL/)
+#### 模板单例类
+
+1. [嵌套类+静态对象](Singleton/SingletonTemplate/SingletonTemplate1.cc)
+2. [pthread_once + atexit](Singleton/SingletonTemplate/SingletonTemplate2.cc)
+
+## [PIMPL设计模式](PIMPL/)(Pointer to Implementation)
 
 ##### 作用：
 
@@ -20,3 +25,10 @@
 2. 降低编译依赖
 3. 只要接口不变, 头文件不变，实现可以随便改造, 可以以一个最小代价完成库的平滑升级
    
+
+## 代理模式(Proxy Pattern)
+
+在代理模式中，一个类代表另一个类的功能。创建具有现有对象的对象，以便向外界提供功能接口。这种类型的设计模式属于结构型模式。
+
+[实现CowString, 让其operator[]能够区分出读写操作](Proxy_Pattern/cow_string.cc)
+
--- mycode/cpp/template/mylog/mylogger.h
+++ mycode/cpp/template/mylog/mylogger.h
@@ -4,49 +4,56 @@
 #include <iostream>
 using namespace log4cpp;
 
-#define suffix(msg)  std::string(msg).append(" [")\
-    .append(__FILE__).append(":").append(__func__)\
-    .append(":").append(to_string(__LINE__))\
-    .append("]").c_str()
+#define suffix(msg)                  \
+    std::string(msg)                 \
+        .append(" [")                \
+        .append(__FILE__)            \
+        .append(":")                 \
+        .append(__func__)            \
+        .append(":")                 \
+        .append(to_string(__LINE__)) \
+        .append("]")                 \
+        .c_str()
 
-
-class Mylogger
-{
-public:
+class Mylogger {
+   public:
     static Mylogger* getInstance(const char* filename);
     static void destroy();
 
-    template<typename... Args>
-        void warn(Args... args) {
-            _mycategory.warn(args...);
-        }
+    template <typename... Args>
+    void warn(Args... args) {
+        _mycategory.warn(args...);
+    }
 
-    template<typename... Args>
-        void error(Args... args) {
-            _mycategory.error(args...);
-        }
+    template <typename... Args>
+    void error(Args... args) {
+        _mycategory.error(args...);
+    }
 
-    template<typename... Args>
-        void debug(Args... args) {
-            _mycategory.debug(args...);
-        }
+    template <typename... Args>
+    void debug(Args... args) {
+        _mycategory.debug(args...);
+    }
 
-    template<typename... Args>
-        void info(Args... args) {
-            _mycategory.info(args...);
-        }
+    template <typename... Args>
+    void info(Args... args) {
+        _mycategory.info(args...);
+    }
 
-private:
-    Mylogger(const char* filename); 
+   private:
+    Mylogger(const char* filename);
     ~Mylogger();
 
-private:
-    Category & _mycategory;
+   private:
+    Category& _mycategory;
     static Mylogger* _pInstance;
 };
 
-#define LogError(msg, ...) Mylogger::getInstance("test.log")->error(suffix(msg), ##__VA_ARGS__)
-#define LogWarn(msg, ...) Mylogger::getInstance("test.log")->warn(suffix(msg), ##__VA_ARGS__)
-#define LogInfo(msg, ...) Mylogger::getInstance("test.log")->info(suffix(msg), ##__VA_ARGS__)
-#define LogDebug(msg, ...) Mylogger::getInstance("test.log")->debug(suffix(msg), ##__VA_ARGS__)
-
+#define LogError(msg, ...) \
+    Mylogger::getInstance("test.log")->error(suffix(msg), ##__VA_ARGS__)
+#define LogWarn(msg, ...) \
+    Mylogger::getInstance("test.log")->warn(suffix(msg), ##__VA_ARGS__)
+#define LogInfo(msg, ...) \
+    Mylogger::getInstance("test.log")->info(suffix(msg), ##__VA_ARGS__)
+#define LogDebug(msg, ...) \
+    Mylogger::getInstance("test.log")->debug(suffix(msg), ##__VA_ARGS__)
