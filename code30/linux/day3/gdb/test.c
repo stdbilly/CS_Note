3fbb0a1816577f3641b012523234301b69890d52
Billy <whb227@163.com>
2019-09-27 23:54:30 +0800

    add

--- code30/linux/day3/gdb/test.c
+++ code30/linux/day3/gdb/test.c
@@ -1,15 +1,22 @@
-#include <stdio.h>
+#include <func.h>
 #define NUM 2+3
 void print(int k)
 {
     printf("I am print k=%d\n",k);
+    printf("I am print1 k=%d\n",k);
+    printf("I am print2 k=%d\n",k);
 }
-int main()
+int main(int argc,char *argv[])
 {
+    if(argc!=3)
+    {
+        printf("error args\n");
+        return -1;
+    }
 	int *p;
 	int arr[3]={1,2,3};
-	int i=NUM*NUM;
-	int j=10;
+	int i=atoi(argv[0]);
+	int j=atoi(argv[1]);
 	arr[2]=4;
 	p=arr;
     print(j);
--- code30/linux/day3/liba/main/main.c
+++ code30/linux/day3/liba/main/main.c
@@ -1,7 +1,9 @@
 #include <func.h>
 
-int multi(int,int);
-int main() {
-	printf("multi=%d\n",multi(3,4));
-	return 0;
+int add(int,int);
+int main()
+{
+    printf("I am main,sum=%d\n",add(3,4));
+    return 0;
 }
+
